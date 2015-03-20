0590915ddaee01e49b722cd0a4c2cd362c120614
Nathan Kinder <nkinder@redhat.com>
2015-03-19 21:21:35 -0700

    Allow Delorean packages to be used for federation setup
    This allows Delorean packages to be optionally used instead of the
    standard RDO packages for the federation setup.

--- rdo-federation-setup/vm-post-cloud-init-rdo.sh
+++ rdo-federation-setup/vm-post-cloud-init-rdo.sh
@@ -34,6 +34,14 @@ yum install -y epel-release
 
 # Set up the rdo-release repo
 yum install -y https://rdo.fedorapeople.org/openstack-juno/rdo-release-juno.rpm
+if [ -n "$USE_DELOREAN" ] ; then
+    wget -O /etc/yum.repos.d/delorean.repo \
+        http://trunk.rdoproject.org/centos70/current/delorean.repo
+    wget -O /etc/yum.repos.d/rdo-kilo.repo \
+        http://copr.fedoraproject.org/coprs/apevec/RDO-Kilo/repo/epel-7/apevec-RDO-Kilo-epel-7.repo
+    wget -O /etc/yum.repos.d/pycrypto.repo \
+        http://copr.fedoraproject.org/coprs/npmccallum/python-cryptography/repo/epel-7/npmccallum-python-cryptography-epel-7 .repo
+fi
 
 # Install packstack
 yum install -y openstack-packstack
